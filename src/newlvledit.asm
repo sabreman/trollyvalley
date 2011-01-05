831843db0a719efd8ee7234f0e4fa075e6ee61f2
Mikko Keinänen <mikko.keinanen@gmail.com>
2011-01-05 19:36:07 +0200

    Dumping standard character set ROM image to RAM. Rendering characters to edit area on screen.

--- src/newlvledit.asm
+++ src/newlvledit.asm
@@ -1,25 +1,41 @@
+atmp                = $02
 ; currently pressed keycode is stored to $00cb
 currkey             = $cb
 
+tmpalo              = $fb
+tmpahi              = $fc 
+; tmpblo/-hi will be used as a pointer to selected character
+; in the character memory
+tmpblo              = $fd
+tmpbhi              = $fe
+
+
 ; free memory:
-; $02
-; $fb-$fe
 ; $02a7-$02ff
 ; $0313
-; $0335-$033b
+; $0337-$033b
 
 ; $03fc-$03ff
 ; $c000-$cfff
 
 ; index of selected character
 curchind            = $0334
+maxchind            = $0335
+minchind            = $0336
 
 scrmemp1            = $0400
 scrmemp2            = $0500
 scrmemp3            = $0600
 scrmemp4            = $0700
+
+; start address of character editor area
+; (magnified character)
+chedstart           = $0608
+
 ; store character set to basic memory
 chrdata             = $3800 ;... $3fff
+; character generator ROM image
+chrrom              = $d000 ;... $dfff
 ; raster register
 ; returns the lower 8 bits of the current raster position
 ; raster is not visible between 51...251
@@ -39,8 +55,9 @@ bgcolor2            = $d023
           *= $8000 
 ; initializing:
 
-          jsr ldchrset 
-          jsr initchrset
+          ;jsr ldchrset 
+          jsr dmpstdch
+          ;jsr initchrset
           jsr clearscr
           jsr printchs
           jsr init
@@ -55,8 +72,21 @@ mainloop
           jmp mainloop 
 ;------------------------------------
 init
+          ; Set the index of current character
+          ; and minimum / maximum character index
           lda #$00
           sta curchind
+          sta minchind
+          lda #$ff
+          sta maxchind
+
+          ; set the pointer to current character
+          ; in the character memory.
+          lda #<chrdata
+          sta tmpblo 
+          lda #>chrdata
+          sta tmpbhi
+
           rts
 ;------------------------------------
 readk
@@ -92,25 +122,142 @@ readkf7   cmp #$03
           ;O
 readko    cmp #$26
           bne readkp
-          dec curchind
-          jsr setselch
+          jsr deccurch
           jmp readkx
 
           ;P
 readkp    cmp #$29
           bne readkx
-          inc curchind
-          jsr setselch
+          jsr inccurch
           jmp readkx
 
 readkx    rts
 
 ;------------------------------------
+deccurch
+          ; do not decrease if minimum index reached
+          lda curchind
+          cmp minchind
+          beq deccurchx
+
+          ; decrease the current character index
+          dec curchind
+
+          ; decrease the current character memory pointer
+          sec                 ; set carry
+          lda tmpblo
+          sbc #$08            ; reduce the pointer for 8 bytes
+          sta tmpblo
+          lda tmpbhi
+          sbc #$00            ; subtract with carry to tmpbhi
+          sta tmpbhi
+
+          jsr setselch
+deccurchx
+          rts
+;------------------------------------
+inccurch 
+          ; do not increase if maximum index reached
+          lda curchind
+          cmp maxchind
+          beq inccurchx
+
+          ; increase the current character index
+          inc curchind
+
+          ; increse the current character memory pointer
+          clc                 ; clear carry
+          lda tmpblo
+          adc #$08            ; increase the pointer for 8 bytes
+          sta tmpblo
+          lda tmpbhi
+          adc #$00            ; add with carry to tmpbhi
+          sta tmpbhi
+
+          jsr setselch
+inccurchx
+          rts
+;------------------------------------
 setselch
-          ; print selected character 
-          ; to screen
+          ; print selected character to screen
           lda curchind
           sta $0518
+
+          ; * character data is stored to 'chrdata'
+          ; * each character consists of 8 bytes
+          ; * character memory offset for selected
+          ;   character is curchind * 8 bytes
+          ; * tmpblo/-hi points to selected character
+          ;   data in the character memory
+
+          ; store the start of memory to print the magnified character
+          lda #<chedstart
+          sta tmpalo
+          lda #>chedstart
+          sta tmpahi
+
+          ; render each bit into a character
+          ; 8 bits / row
+
+          ldy #$00            ; y is index to character data memory
+                              ; need y for indirect access
+setselch1
+          ; tmpblo/-hi points to selected character's data
+          ; in the character memory
+          lda (tmpblo),y      ; load (indirectly) a byte of character data 
+          sta atmp
+          jsr rendbyte
+
+          ; set the start of next row in the character editor
+          ; screen memory ( 40 chars - 8 chars -> add #$20)
+          clc
+          lda tmpalo
+          adc #$28
+          sta tmpalo
+          lda tmpahi
+          adc #$00
+          sta tmpahi
+
+          iny                 ; next byte in memory, next row in editor
+          cpy #$08
+          bne setselch1
+
+          rts
+;------------------------------------
+rendbyte    
+          ; render a byte (bit by bit) from right to left 
+          ; to given position in screen memory
+
+          ; atmp contains byte to be rendered
+          ; tmpalo/-hi contains the start byte in screen memory
+
+          ; store y to stack
+          tya                 ; y -> acc
+          pha                 ; acc -> stack
+
+          ldy #$00            ; index to screen memory
+                              ; need y for indirect access
+
+rendbyte1
+          asl atmp            ; roll a bit from left to carry
+          bcs rendbyte2       ; draw char if carry was set
+
+          lda #$20            ; empty space
+          jmp rendbyte3
+
+rendbyte2 
+          lda curchind; #$04
+
+rendbyte3 
+          sta (tmpalo),y      ; store empty or mark to char editor screen mem
+          iny
+          cpy #$08            ; was this last bit to be rendered?
+          bne rendbyte1       ; continue if not...
+
+          ; restore y from stack
+          pla                 ; stack -> acc
+          tay                 ; acc -> y
+
           rts
 ;------------------------------------
 tglchrst 
@@ -253,6 +400,59 @@ printchs1
           txa
           inx
           bne printchs1
+          rts
+;------------------------------------
+dmpstdch 
+          ; dumps standard character set from 
+          ; character generator ROM to 
+          ; RAM (chrdata) for editing
+
+          ; turn off interrupts
+          sei
+
+          ; switch out I/O registers and switch
+          ; in character memory to $d000
+          lda $01
+          and #$fb            ; 11111011
+          sta $01
+
+          lda #<chrrom
+          sta tmpalo
+          lda #>chrrom
+          sta tmpahi
+
+          lda #<chrdata
+          sta tmpblo
+          lda #>chrdata
+          sta tmpbhi
+
+          ; character memory is now in $d000-$dfff
+          ; dump the character memory
+          ldx #$08  ; go through 8 pages of memory
+          ldy #$00
+dmpstdch1
+          lda (tmpalo),y
+          sta (tmpblo),y
+          iny
+          bne dmpstdch1
+          ;ldy #$00 ; this is alredy 00
+
+          ; offset a page to tmpalo/-hi
+          ; (increase high byte by one)
+          inc tmpahi
+          inc tmpbhi
+          dex
+          bne dmpstdch1
+
+          ; switch I/O registers back to $d000 
+          ; and character rom out
+          lda $01
+          ora #$04  ; 00000100
+          sta $01
+
+          ; switch back interrupts
+          cli 
+
           rts
 ;------------------------------------
 
